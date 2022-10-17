c10b8a4158703ce71a56a10178e3926c871df47e
Emanuel <emanuelpeg@yahoo.com.ar>
2022-10-17 01:50:46 -0300

    ejercicio orientado a objetos

---
+++ objetos/clase7_bis/src/main/scala/json/ArrayJson.scala
@@ -0,0 +1,10 @@
+package json
+
+import scala.collection.mutable.ListBuffer
+
+class ArrayJson extends ElementJson {
+
+  def add(elementJson: ElementJson) = ???
+
+  override def toString: String = ???
+}
---
+++ objetos/clase7_bis/src/main/scala/json/ObjectJson.scala
@@ -0,0 +1,10 @@
+package json
+
+import scala.collection.mutable.ListBuffer
+
+class ObjectJson extends ElementJson {
+
+  def add(elementJson: ElementJson) = ???
+
+  override def toString: String = ???
+}
